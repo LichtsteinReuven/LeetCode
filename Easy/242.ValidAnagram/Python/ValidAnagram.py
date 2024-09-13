c2170843d5155265ad1afe04fa790cfab68b2ed4
Reuven <reuven.lichtstein@mail.huji.ac.il>
2024-09-13 04:18:06 +0300

    Added python solution to problem #242

---
+++ Easy/242.ValidAnagram/Python/ValidAnagram.py
@@ -0,0 +1,47 @@
+from collections import Counter
+
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+
+Constraints:
+    1 <= s.length, t.length <= 5 * 10^4
+    s and t consist of lowercase English letters.
+"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        words_count = Counter(s)
+        for char in t:
+            if char not in words_count:
+                return False
+            if words_count[char] < 1:
+                return False
+            words_count[char] -= 1
+        return True
+
+
+sol = Solution()
+print(sol.isAnagram("anagram", "nagaram"))  # True
+
+print(sol.isAnagram("rat", "car"))  # False
+
+print(sol.isAnagram("a", "ab"))  # False
+
+print(sol.isAnagram("a", "a"))  # True
+
+print(sol.isAnagram("a", "b"))  # False
